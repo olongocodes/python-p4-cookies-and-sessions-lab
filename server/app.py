8b7c8e1d80f71f20b9a24fa5fd25b4a3a961998f
Kevin <kevinolongo9@gmail.com>
2024-01-18 08:32:18 +0300

    implement session

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
+from sqlalchemy.orm import Session
 
 from models import db, Article, User
 
@@ -22,13 +21,32 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
     pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # Initialize page_views in session if not present
+    session['page_views'] = session.get('page_views', 0)
 
-    pass
+    # Increment page_views by 1 for every request
+    session['page_views'] += 1
+
+    # Check if the user has viewed 3 or fewer pages
+    if session['page_views'] <= 3:
+        with Session(db.engine) as db_session:
+            article = db_session.get(Article, id)
+            if article:
+                return jsonify({
+                    'author': article.author,
+                    'title': article.title,
+                    'content': article.content,
+                    'preview': article.preview,
+                    'minutes_to_read': article.minutes_to_read,
+                    'date': article.date
+                })
+
+    # If the user has viewed more than 3 pages, return a 401 unauthorized response
+    return jsonify({'message': 'Maximum pageview limit reached'}), 401
 
 if __name__ == '__main__':
     app.run(port=5555)
